498153697a8d86d6c4d38eb0d22cb6a12bd8d6e0
Krishantha Dinesh <kdinesh@virtusa.com>
2023-05-28 16:32:14 +0530

    add decompress logic

--- jsobject-compressor/data.js
+++ jsobject-compressor/data.js
@@ -1,4 +1,7 @@
 const graph = {
   //add your data here
+  _id: {
+    $oid: '633081f2fd1bf64f02d99794',
+  },
 };
 module.exports = graph;
--- jsobject-compressor/index.js
+++ jsobject-compressor/index.js
@@ -5,3 +5,5 @@ const bufferData = Buffer.from(JSON.stringify(data));
 console.log(` size before compress ${bufferData.length / 1024} kb`);
 const compressedData = zlib.brotliCompressSync(bufferData);
 console.log(` size after compress ${compressedData.length / 1024} kb`);
+const decompressedData = zlib.brotliDecompressSync(compressedData);
+console.log(` size after Decompress ${decompressedData.length / 1024} kb`);
